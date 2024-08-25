dff08e90a1f26d5b0ae58e626dc17e5a36e88a99
Aloy <aloy@fantasywelt.net>
2024-08-25 22:35:28 +0200

    Revert "Core/Players: Fixed trait configs getting into invalid state after talent tree changes between client patches"
    This reverts commit 52a2ad8b1cee5bc77120fd5d2724402600bb394f.

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1775,7 +1775,6 @@ CREATE TABLE `character_trait_entry` (
   `traitNodeId` int NOT NULL,
   `traitNodeEntryId` int NOT NULL,
   `rank` int NOT NULL DEFAULT '0',
-  `grantedRanks` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`guid`,`traitConfigId`,`traitNodeId`,`traitNodeEntryId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
