e96e0644b67058d2d42089e19004a8eda26ea5ec
Aloy <aloy@fantasywelt.net>
2024-10-12 21:33:29 +0200

    Scripts/Spells: Fix dk talent Icy Talons from aquadeus

--- src/server/scripts/Spells/spell_dk.cpp
+++ src/server/scripts/Spells/spell_dk.cpp
@@ -84,6 +84,7 @@ enum DeathKnightSpells
     SPELL_DK_RUNIC_RETURN                       = 61258,
     //SPELL_DK_SLUDGE_BELCHER                     = 207313, // dropped in BfA
     //SPELL_DK_SLUDGE_BELCHER_SUMMON              = 212027, // dropped in BfA
+    SPELL_DK_SMOTHERING_OFFENSE                 = 435005,
     SPELL_DK_SOUL_REAPER                        = 343294,
     SPELL_DK_SOUL_REAPER_DAMAGE                 = 343295,
     SPELL_DK_DEATH_STRIKE_ENABLER               = 89832, // Server Side
@@ -805,18 +806,33 @@ class spell_dk_howling_blast : public SpellScript
 // 194878 - Icy Talons
 class spell_dk_icy_talons : public AuraScript
 {
-    bool CheckProc(AuraEffect const* /*aurEff*/, ProcEventInfo& eventInfo)
+    bool CheckProc(AuraEffect const* /*aurEff*/, ProcEventInfo const& eventInfo) const
     {
         if (Spell const* procSpell = eventInfo.GetProcSpell())
-            if (Optional<int32> cost = procSpell->GetPowerTypeCostAmount(POWER_RUNIC_POWER))
-                return cost > 0;
-
+            return procSpell->GetPowerTypeCostAmount(POWER_RUNIC_POWER) > 0;
         return false;
     }
-
     void Register() override
     {
         DoCheckEffectProc += AuraCheckEffectProcFn(spell_dk_icy_talons::CheckProc, EFFECT_0, SPELL_AURA_PROC_TRIGGER_SPELL_WITH_VALUE);
+    }
+};
+
+// 194879 - Icy Talons
+class spell_dk_icy_talons_buff : public SpellScript
+{
+    bool Validate(SpellInfo const* /*spellInfo*/) override
+    {
+        return ValidateSpellInfo({ SPELL_DK_SMOTHERING_OFFENSE });
+    }
+    void HandleSmotheringOffense(WorldObject*& target) const
+    {
+        if (!GetCaster()->HasAura(SPELL_DK_SMOTHERING_OFFENSE))
+            target = nullptr;
+    }
+    void Register() override
+    {
+        OnObjectTargetSelect += SpellObjectTargetSelectFn(spell_dk_icy_talons_buff::HandleSmotheringOffense, EFFECT_1, TARGET_UNIT_CASTER);
     }
 };
 
@@ -1230,6 +1246,7 @@ void AddSC_deathknight_spell_scripts()
     RegisterSpellScript(spell_dk_glyph_of_scourge_strike_script);
     RegisterSpellScript(spell_dk_howling_blast);
     RegisterSpellScript(spell_dk_icy_talons);
+    RegisterSpellScript(spell_dk_icy_talons_buff);
     RegisterSpellScript(spell_dk_mark_of_blood);
     RegisterSpellScript(spell_dk_obliteration);
     RegisterSpellScript(spell_dk_permafrost);
