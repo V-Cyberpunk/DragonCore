435f105d0889957a5adb96f11be0934ce37ba4f5
Aloy <aloy@fantasywelt.net>
2024-11-10 20:25:14 +0100

    sql for SPELL_EFFECT_TELEPORT_UNITS from meji

---
+++ sql/updates/world/2024_11_10_01_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_target_position` WHERE (`EffectIndex`=0 AND `ID`=102930);
+INSERT INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`, `VerifiedBuild`) VALUES
+(102930, 0, 870, 3138.8701171875, -721.25, 325, 57388); -- Spell: 102930 (Teleport Player to Crash Site) Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
