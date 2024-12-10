463f2191ca531ae1a2705a95daaa5261746b0db2
Aloy <aloy@fantasywelt.net>
2024-12-10 19:17:43 +0100

    Spells/DH: Fix proc of Serrated Glaive from aquadeus

---
+++ sql/updates/world/2024_12_10_01_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId` IN (390154);
+INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
+(390154,0x00,107,0x00000020,0x00100000,0x00000000,0x00000000,0x0,0x0,0x5,0x2,0x0,0x2,0x0,0,0,0,0); -- Serrated Glaive
